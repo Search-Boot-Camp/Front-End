e247f497e2b56d51d0b0367d41f9b815300e9676
tnghk <tnghk9611@naver.com>
2022-02-15 17:31:28 +0900

    header 검색창 삭제

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,25 +1,9 @@
-import {Navbar, Nav, Container, Form, Button} from 'react-bootstrap';
-import React, {useState} from 'react';
-import { useNavigate } from 'react-router-dom';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import React from 'react';
 import './Header.css';
 import logo from './../../img/logo.png';
-import find from './../../img/find.png';
 
 const Header = () => {
-
-    const [search, setSearch] = useState("");
-    const navigate = useNavigate();
-
-    const onSubmit = () => {
-        {
-            // https://velog.io/@soryeongk/ReactRouterDomV6
-            navigate('/search',{search})
-        }
-    };
-
-    const onChange = (event) => {
-        setSearch(event.target.value);
-    }
 
     return (
         <div>
@@ -34,12 +18,6 @@ const Header = () => {
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/search">부트 캠프 찾기</Nav.Link>
                         </Nav>
-                        <Form className='form' onSubmit={onSubmit}>
-                            <Form.Control type="string" onChange={onChange} placeholder="찾고 싶은 부트캠프를 검색하세요! "/>
-                            <Button variant="secondary" type="submit" id="findbt">
-                                <img alt="" src={find} width="23" height="24"/>
-                            </Button>
-                        </Form>
                     </Container>
                 </Navbar>
             </div>

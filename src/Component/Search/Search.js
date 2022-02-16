5d8347a298b99c1e9020aff6146f2bb7591b230f
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-02-16 19:29:38 +0900

    merge

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -5,6 +5,7 @@ import Footer from '../Footer/Footer';
 import ImgBox from '../ImgBox/ImgBox';
 import {Form} from 'react-bootstrap';
 import axios from 'axios'
+import find from './../../img/find.png';
 
 const Search = () => {
 
@@ -62,9 +63,9 @@ const Search = () => {
                                 placeholder="찾고 싶은 부트캠프를 검색하세요!"
                                 onKeyPress={onKeyPressHandler}>
                             </input>
-                            <Button variant="secondary" onClick = {getData} id="findbt">
+                            <button variant="secondary" onClick = {getData} id="findbt">
                                 <img alt="" src={find} width="23" height="23"/>
-                            </Button>
+                            </button>
                         </div>
                     </div>
 

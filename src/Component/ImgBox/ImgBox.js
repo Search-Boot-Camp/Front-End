8a660d415bae91ad8f5d0787fa0e4da06828fbb1
tnghk <tnghk9611@naver.com>
2022-02-16 21:57:32 +0900

    대체이미지 속성 추가

--- src/Component/ImgBox/ImgBox.js
+++ src/Component/ImgBox/ImgBox.js
@@ -1,34 +1,47 @@
 import './ImgBox.css';
 import React from 'react';
-// import bootData from './bootdata.js';
+import default_Img from "../../img/site_logo.png";
+import {Link} from 'react-router-dom';
 
-function BootCard(props)
-{
-  return (
-    <div className="col-md-4">
-      <div className="bootimg-box">
-        <div className="img-info">
-          <img alt="없음" src={'./'+(props.boots.image_id)+'.png'} width="100%" height="100%"/>
+const onErrorImg = (e) => {
+    e.target.src = default_Img;
+ } 
+
+function BootCard(props) {
+    return (
+        <div className="col-md-4">
+            <div className="bootimg-box">
+                <div className="img-info">
+                    <Link to={{
+                            pathname: `detail/`,
+                            state: { data: props.boots },
+                        }}>
+                        <img alt="없음" src={'./' + (props.boots.image_id) + '.png'} onError={onErrorImg} width="100%" height="100%"/>
+                    </Link>
+                </div>
+                <h6 id="boot-brand-name">
+                    {props.boots.bootcamp_name}
+                </h6>
+            </div>
         </div>
-        <h6 id="boot-brand-name"> { props.boots.bootcamp_name } </h6>
-      </div>
-    </div>
-  );
+    );
 }
 
-function ImgBox(props) 
-{ 
-  return (
-    <div>
-      <div className="container" id="bootimg-container">
-        <div className="row">
-        {
-          props.data && props.data.map((item,num)=>
-          { return <BootCard boots={props.data[num]} num={num} key={num}/> })
-        }
+function ImgBox(props) {
+    return (
+        <div>
+            <div className="container" id="bootimg-container">
+                <div className="row">
+                    {
+                        props.data && props
+                            .data
+                            .map((item, num) => {
+                                return <BootCard boots={props.data[num]} num={num} key={num}/>
+                            })
+                    }
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     );
 }
 

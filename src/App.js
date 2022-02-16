7b7eb943889ce2eaa675bb7c25695bcb5ae8eb8f
hyerini <rinlee2001@naver.com>
2022-02-17 05:02:29 +0900

    header 링크 연결

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ImgBox from './Component/ImgBox/ImgBox';
 import Details from './Component/Details/Details';
 import SearchPage from './Component/Search/Search';
 import IntroducePage from './Component/Introduce/Introduce';
-import Ad from './Component/Ad/Ad';
+// import Ad from './Component/Ad/Ad';
 import { Route, Routes } from 'react-router-dom';
 // import {logo} from './site_logo.png';
 
@@ -30,7 +30,6 @@ function MainPage()
                     <Select/>
                 </div>
 
-<<<<<<< HEAD
                 <div>
                     <ImgBox/>
                 </div>
@@ -42,23 +41,11 @@ function MainPage()
             <div id="error">
                 <div> 
                     <p>여러분<br/>
-                    지금당장<br/>화면을<br/>전환하고<br/>부트캠프를
+                    지금당장<br/>화면을<br/>180도<br/>돌리고<br/>부트캠프를
                     <br/>검색하세요!!!!</p>
                 </div>
             </div>
-=======
-        <div>
-            <ImgBox/>
         </div>
-        <div>
-            <Ad />
-        </div>
-        
-        <div>
-            <Footer/>
-        </div>
->>>>>>> c43e369c399d268741328624fcde7df0ec945524
-    </div>
     );
 }
 

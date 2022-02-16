13ebd94b51f0bf3881460df42139a99689aca662
tnghk <tnghk9611@naver.com>
2022-02-16 21:56:38 +0900

    search 수정 (실시간x)

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -5,6 +5,7 @@ import Footer from '../Footer/Footer';
 import ImgBox from '../ImgBox/ImgBox';
 import {Form} from 'react-bootstrap';
 import axios from 'axios'
+import find from './../../img/find.png';
 
 const Search = () => {
 
@@ -14,10 +15,12 @@ const Search = () => {
         setSearch(event.target.value);
      };
 
-    // 검색창에서 돋보기버튼 눌렀을 때
-    const onSubmit = () => { { console.log(search) } };
-
-    useEffect(() => {
+    const onKeyPressHandler=(e)=>{
+        if(e.key == 'Enter'){
+            getData();
+        }
+    }
+    const getData = () => {
         let completed = false;
 
         if (search === " " || search === "") {
@@ -26,8 +29,10 @@ const Search = () => {
         }
 
         async function get() {
-            // http://ec2-13-209-65-110.ap-northeast-2.compute.amazonaws.com:8000/api/bootcamp/search?search=html
-            const result = await axios(`/api/bootcamp/search?search=${search}`)
+            // http://ec2-13-209-65-110.ap-northeast-2.compute.amazonaws.com:8000/api/boot
+            // camp/search?search=html
+            const result = await axios(`http://ec2-13-209-65-110.ap-northeast-2.compute.amazonaws.com:8000/api/bootcamp/search?search=${search}`)
+
             if (!completed) {
                 setData(result.data);
                 // data 출력
@@ -40,8 +45,7 @@ const Search = () => {
             completed = true
 
         }
-    }, [search])
-    
+    }
     return (
         <div>
             <Header/>
@@ -51,12 +55,18 @@ const Search = () => {
                         <img className="main-logo" src="site_logo.png" alt=""/>
                     </div>
                     <div className="form-f-box">
-                        <Form id="form" onSubmit={onSubmit}>
-                            <Form.Control
+                        <div id="form">
+                            <input
+                                className ="form-input"
                                 type="string"
                                 onChange={onChange}
-                                placeholder="찾고 싶은 부트캠프를 검색하세요!"/>
-                        </Form>
+                                placeholder="찾고 싶은 부트캠프를 검색하세요!"
+                                onKeyPress={onKeyPressHandler}>
+                            </input>
+                            <button variant="secondary" onClick = {getData} id="findbt">
+                                <img alt="" src={find} width="23" height="23"/>
+                            </button>
+                        </div>
                     </div>
 
                     <div id="search-box">
